49c291efc713eae0c300dc00513095b38c73e220
Aqeeb Imtiaz Harun <aqeebimtiaz@gmail.com>
2021-11-17 00:03:35 +0600

    [ADD] upto vid 5

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,18 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Text('Hola'),
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('My First Flutter App'),
+        centerTitle: true,
+      ),
+      body: Center(
+          child: Text("hola"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Text('click'),
+        onPressed: null,
+      )
+    ),
   ));
 }
